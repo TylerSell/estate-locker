c502948d6a39f83186f814131aa88318cf89fb79
TylerSell <tylerselldesign@gmail.com>
2019-10-22 14:42:46 -0500

    building post routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,19 +2,39 @@ class UserControler < ApplicationController
 
     get '/signup' do
         if logged_in?
-            redirect '/tasks'
+            redirect '/family_members'
         else
             erb :'users/signup'
         end
     end
 
+    post '/signup' do
+        if params[:username] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            @user = User.create(:username => params[:username], :password => params[:password])
+            session[:user_id] = @user.id
+            redirect '/family_members'
+        end
+    end
+
     get '/login' do 
         if logged_in?
-            redirect '/tasks'
+            redirect '/family_members'
         else
             erb :'users/login'
         end
     end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/family_members'
+        else
+            redirect '/login'
+        end
+      end
 
     get '/logout' do 
         if session[:user_id] != nil
@@ -23,6 +43,6 @@ class UserControler < ApplicationController
             redirect '/'
         end
     end
-    
+
 
 end
