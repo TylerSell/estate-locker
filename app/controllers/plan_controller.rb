a9d272d1484462bcc6a57ab5888247942ce665d1
TylerSell <tylerselldesign@gmail.com>
2019-10-29 13:09:44 -0500

    updating routes

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -7,6 +7,7 @@ class PlanController < ApplicationController
     end
 
     post '/plans/new' do 
+        redirect_if_not_logged_in
         @user = current_user
         @plan = @user.family_members.plan.create(:type => params[:type], :company => params[:company], :account_number => params[:account_number], :contact_number => params[:contact_number])
         redirect '/family_members'
@@ -15,13 +16,13 @@ class PlanController < ApplicationController
     get '/plans/:id' do 
         redirect_if_not_logged_in
         @plan = Plan.find_by_id(params[:id])
-        erb :'/family_members/show'
+        erb :'/plans/show'
     end
 
     get '/plans/:id/edit' do 
         redirect_if_not_logged_in
         @family_member = FamilyMember.find_by_id(params[:id])
-        erb :'family_members/edit'
+        erb :'plans/edit'
     end
 
     patch '/plans/:id' do 
@@ -34,7 +35,7 @@ class PlanController < ApplicationController
     get '/plans/:id/delete' do 
         redirect_if_not_logged_in
         @family_member = FamilyMember.find_by_id(params[:id])
-        erb :'family_members/delete'
+        erb :'plans/delete'
     end
 
     delete '/plans/:id' do 
