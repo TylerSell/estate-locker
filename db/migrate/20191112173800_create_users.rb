93c8501c80ede6427554b8fbb412f4df5beb82c0
TylerSell <tylerselldesign@gmail.com>
2019-11-12 11:54:10 -0600

    building tables

--- db/migrate/20191112173800_create_users.rb
+++ db/migrate/20191112173800_create_users.rb
@@ -1,4 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
+    create_table :users do |table|
+      table.string :first_name
+      table.string :last_name
+      table.string :username
+      table.string :password_digest
   end
 end
--- db/migrate/20191112173910_create_plans.rb
+++ db/migrate/20191112173910_create_plans.rb
@@ -1,4 +1,13 @@
 class CreatePlans < ActiveRecord::Migration[6.0]
   def change
+    create_table :plans do |table|
+      table.string :type
+      table.string :company
+      table.string :account_number
+      table.string :contact_number
+      table.string :beneficiary
+      table.string :notes
+      table.integer :family_member_id
+      table.integer :user_id
   end
 end
